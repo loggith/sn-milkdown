5c68cb219f8381675affe44ed144a56a246f8b33
log <log126@163.com>
2024-07-19 09:34:52 +0800

    1.0.0

--- src/components/CustomEditor.tsx
+++ src/components/CustomEditor.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import snApi from "sn-extension-api";
+import { MilkdownEditorWrapper } from './Milkdown';
 
 const CustomEditor = () => {
   const spacing = snApi.extensionMeta?.spacing || 'Default';
 
   return (
     <div className={'main__' + spacing}>
-      MilkdownEditorWrapper
+      <MilkdownEditorWrapper/>
     </div>
   );
 }
